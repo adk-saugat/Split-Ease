9821665b0d7737ce682f0ed587510dde58414206
adk-saugat <adhikarisaugat34@gmail.com>
2024-12-08 12:36:02 -0600

    completing signup page design

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router"
 const SignUpPage = () => {
   const navigate = useNavigate()
   return (
-    <div className="login-container">
-      <h2 className="login-header">Sign Up</h2>
-      <form className="login-form">
+    <div className="signup-container">
+      <h2 className="signup-header">Sign Up</h2>
+      <form className="signup-form">
         <input
           className="email-input"
           type="text"
@@ -25,11 +25,11 @@ const SignUpPage = () => {
           placeholder="Password"
           required
         />
-        <button className="login-btn">Login</button>
+        <button className="signup-btn">Login</button>
         <div className="signup-Header">
-          Don't have an account?
-          <a onClick={() => navigate("/sign-up")} className="signup-link">
-            Sign Up
+          Already have an Account?
+          <a onClick={() => navigate("/login")} className="login-link">
+            Login
           </a>
         </div>
       </form>
